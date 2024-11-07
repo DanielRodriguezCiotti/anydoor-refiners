a02ce8d13594e2c7c6c6b62ab5e5dd680804636e
Daniel Rodriguez <darc2203@gmail.com>
2024-11-07 10:51:19 +0100

    fix: dinov2

--- src/anydoor_refiners/dinov2.py
+++ src/anydoor_refiners/dinov2.py
@@ -4,16 +4,23 @@ from refiners.foundationals.dinov2 import DINOv2_giant
 from torch import device as Device, dtype as DType
 
 
+class Normalize(fl.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        mean = torch.tensor([0.485, 0.456, 0.406]).unsqueeze(0).unsqueeze(-1).unsqueeze(-1).to( device=x.device, dtype=x.dtype)
+        std = torch.tensor([0.229, 0.224, 0.225]).unsqueeze(0).unsqueeze(-1).unsqueeze(-1).to( device=x.device, dtype=x.dtype)
+        return (x - mean) / std
+
 class DINOv2Encoder(fl.Chain):
     def __init__(
         self,
         device: Device | str | None = None,
         dtype: DType | None = None,
     ) -> None:
-        mean = torch.tensor([0.485, 0.456, 0.406]).unsqueeze(0).unsqueeze(-1).unsqueeze(-1)
-        std = torch.tensor([0.229, 0.224, 0.225]).unsqueeze(0).unsqueeze(-1).unsqueeze(-1)
         super().__init__(
-            fl.Lambda(lambda x: (x - mean) / std),
+            Normalize(),
             DINOv2_giant(device=device, dtype=dtype),
             fl.Linear(1536, 1024, device=device, dtype=dtype),
         )
